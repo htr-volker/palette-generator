f47d254211d81b277bac5c2979f1ae17d8c40482
htr-volker <htr.volker@gmail.com>
2021-06-18 14:40:14 +0100

    reverting namespace specification to original

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-echo "Namespace: ${NAMESPACE}"
-kubectl create namespace $NAMESPACE || true
-kubectl apply -f k8s --namespace=$NAMESPACE
+if [ -z $BRANCH_NAME ]
+do
+    BRANCH_NAME="default"
+done
+echo "Namespace: ${BRANCH_NAME}"
+kubectl create namespace $BRANCH_NAME || true
+kubectl apply -f k8s --namespace=$BRANCH_NAME
