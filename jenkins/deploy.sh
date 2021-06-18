5e690aca4ad3027c7100956032e8f0ca745570e8
htr-volker <htr.volker@gmail.com>
2021-06-18 14:41:48 +0100

    fixed if statement in seploy script

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ -z $BRANCH_NAME ]
-do
+if [ -z $BRANCH_NAME ];
+then
     BRANCH_NAME="default"
-done
+fi
 echo "Namespace: ${BRANCH_NAME}"
 kubectl create namespace $BRANCH_NAME || true
 kubectl apply -f k8s --namespace=$BRANCH_NAME
